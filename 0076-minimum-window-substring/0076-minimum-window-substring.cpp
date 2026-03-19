db991e6ccf7b24f0fd30b56befa1fdcc2547b1c7
NitinKushwahcodes <xyzcoding06@gmail.com>
2026-03-19 22:46:26 +0530

    Time: 0 ms (100%), Space: 11.2 MB (93.09%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,50 +1,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-
-        vector<int> freq(128, 0);
-
-        for(char c: t){
-            freq[c]++;
-        }
-
-        int left = 0;
-        int right = 0;
+        vector<int>freq(128,0);
+        for(char ch: t) freq[ch]++;
         int required = t.size();
-
-        int minLen = INT_MAX;
         int start = 0;
-
+        int min_len = INT_MAX;
+        int right = 0, left = 0;
         while(right < s.size()){
-
             if(freq[s[right]] > 0){
                 required--;
             }
-
-            freq[s[right]]--;
-            right++;
-
+            freq[s[right++]]--;
             while(required == 0){
-
-                if(right - left < minLen){
-                    minLen = right - left;
+                if(right-left < min_len){
+                    min_len = right-left;
                     start = left;
                 }
-
                 freq[s[left]]++;
-
                 if(freq[s[left]] > 0){
                     required++;
                 }
-
                 left++;
             }
-        }
-
-        if(minLen == INT_MAX){
-            return "";
-        }
-
-        return s.substr(start, minLen);
+        } 
+        if(min_len == INT_MAX) return "";
+        return s.substr(start, min_len);
     }
 };
