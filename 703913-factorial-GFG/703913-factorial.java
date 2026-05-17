b2a85b0415e927d22c05ac1e0c9319360a2368a8
Nitin Kushwah <xyzcoding06@gmail.com>
2026-05-17 12:46:29 +0530

    Added solution - LeetHub

---
+++ 703913-factorial-GFG/703913-factorial.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int factorial(int n) {
+        // code here
+        int ans=1;
+        for(int i=2; i<=n; i++){
+            ans=ans*i;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
