22fab27e888da43fa135c80dd7ef820a1e0aaffe
Nitin Kushwah <xyzcoding06@gmail.com>
2026-06-02 21:59:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub - Time: O(m + n), Space: O(1)

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int idx = n+m-1;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] >= nums2[j]){
+                nums1[idx--] = nums1[i--];
+            }
+            else{
+                nums1[idx--] = nums2[j--];
+            }
+        }
+        while(j >= 0){
+            nums1[idx--] = nums2[j--];
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
