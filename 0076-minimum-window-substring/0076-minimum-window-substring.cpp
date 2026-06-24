ac51b8fb04954fc87bf4dba8b5ad51054f881085
Nitin Kushwah <xyzcoding06@gmail.com>
2026-06-24 21:56:40 +0530

    Time: 1 ms (83.79%), Space: 11.4 MB (64.54%) - LeetHub - Time: O(n + m), Space: O(1)

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -28,3 +28,7 @@ public:
         return s.substr(start, min_len);
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
