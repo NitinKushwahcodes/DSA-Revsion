45b1b56d444fabff1bc65967235d69b6f3a087ef
Nitin Kushwah <xyzcoding06@gmail.com>
2026-07-12 13:18:26 +0530

    Added solution - LeetHub

--- 878537-implement-lower-bound-GFG/878537-implement-lower-bound.cpp
+++ 878537-implement-lower-bound-GFG/878537-implement-lower-bound.cpp
@@ -4,18 +4,16 @@ class Solution {
         // code here
         int low = 0;
         int high = arr.size()-1;
-        int ans = arr.size();
         while(low <= high){
-            int mid = low+(high-mid)/2;
+            int mid = low+(high-low)/2;
             if(arr[mid] >= target){
-                ans = mid;
                 high = mid-1;
             }
             else{
                 low = mid+1;
             }
         }
-        return ans;
+        return low;
     }
 };
 
