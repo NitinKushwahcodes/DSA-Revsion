1358947fb10a218fdf71ab6bf1664a6950f0fbba
Nitin Kushwah <xyzcoding06@gmail.com>
2026-07-12 13:22:02 +0530

    Added solution - LeetHub

---
+++ 878949-implement-upper-bound-GFG/878949-implement-upper-bound.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        // code here
+        int low = 0;
+        int high = arr.size()-1;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            if(arr[mid] > target){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
