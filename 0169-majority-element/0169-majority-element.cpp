c5f65072f49c77034cddbdad7bc1a92f9903043b
Nitin Kushwah <xyzcoding06@gmail.com>
2026-07-13 17:39:21 +0530

    Time: 0 ms (100%), Space: 28.1 MB (68.23%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int mj = -1;
+        int vote = 0;
+        for(int num: nums){
+            if(vote == 0)mj = num;
+            if(mj == num) vote++;
+            else vote--;
+        }
+        return mj;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
